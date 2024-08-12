b1831c2155ef6ab4c8c24f2d07610b2a898f8d7b
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-08-12 22:15:30 +0530

    Add files via upload

--- Sum_product.py
+++ Sum_product.py
@@ -5,15 +5,15 @@ def sum_and_prod(L):
     sum of all elements in L and the second value 
     is the product of all elements in L 
     """
-    s, p = 0, 1
+    s, p = 0, 1 ## product has to be 1, other wise it will become 0
     
     for i in L:
       s += i
       p *= i
     return (s, p)
 
-# print(sum_and_prod([1,2,3,4]))   # prints (10, 24)
-# print(sum_and_prod([12,6,2,7]))   # prints (27, 1008)
+print(sum_and_prod([1,2,3,4]))   # prints (10, 24)
+print(sum_and_prod([12,6,2,7]))   # prints (27, 1008)
 
 
 #############################################
@@ -59,48 +59,58 @@ def sublist_sum(L):
     """ L is a list whose elements are lists with int elements
     Returns the sum of all int elements. """
     # your code here
+    sum_ = 0
+    sum_1 = 0
+    for i in L:
+        for j in i:
+            sum_ += j
+        # sum_1 += sum_  ## returns 21
+    # sum_1 += sum_   ## returns 18
+    # return sum_1
+    return sum_   ##returns 18
+            
 
-# print(sublist_sum([[1,2], [4,5,6]])) # prints 18
+print(sublist_sum([[1,2], [4,5,6]])) # prints 18
 
-
+###### 12 august, 10:15 PM
 #############################################
 ################## ANSWERS TO AT HOME ####################
 #############################################
 
-def max_of_both(n, f1, f2):
-    """ n is an int
-        f1 and f2 are functions that take in an int and return a float
-    Applies f1 and f2 on all numbers between 0 and n (inclusive). 
-    Returns the maximum value of all these results.
-    """
-    # your code here
-    maxval = f1(0)
-    for i in range(n+1):
-        if f1(i) > maxval:
-            maxval = f1(i)
-        if f2(i) > maxval:
-            maxval = f2(i)
-    return maxval
+# def max_of_both(n, f1, f2):
+#     """ n is an int
+#         f1 and f2 are functions that take in an int and return a float
+#     Applies f1 and f2 on all numbers between 0 and n (inclusive). 
+#     Returns the maximum value of all these results.
+#     """
+#     # your code here
+#     maxval = f1(0)
+#     for i in range(n+1):
+#         if f1(i) > maxval:
+#             maxval = f1(i)
+#         if f2(i) > maxval:
+#             maxval = f2(i)
+#     return maxval
 
-# print(max_of_both(2, lambda x:x-1, lambda x:x+1))  # prints 3
-# print(max_of_both(10, lambda x:x*2, lambda x:x/2))  # prints 20
+# # print(max_of_both(2, lambda x:x-1, lambda x:x+1))  # prints 3
+# # print(max_of_both(10, lambda x:x*2, lambda x:x/2))  # prints 20
 
 
-def sublist_sum(L):
-    """ L is a list whose elements are lists with int elements
-    Returns the sum of all int elements. """
-    ## One way by using the sum function over the sublist
-    tot = 0
-    for subL in L:
-        tot += sum(subL)
-    return tot
-    ## Alternate way by nesting a for loop that 
-    ## iterates over the sublist's int elements
-    tot = 0
-    for subL in L:
-        for e in subL:
-            tot += e
-    return tot
+# def sublist_sum(L):
+#     """ L is a list whose elements are lists with int elements
+#     Returns the sum of all int elements. """
+#     ## One way by using the sum function over the sublist
+#     tot = 0
+#     for subL in L:
+#         tot += sum(subL)
+#     return tot
+#     ## Alternate way by nesting a for loop that 
+#     ## iterates over the sublist's int elements
+#     tot = 0
+#     for subL in L:
+#         for e in subL:
+#             tot += e
+#     return tot
     
 
-# print(sublist_sum([[1,2], [4,5,6]])) # prints 18
+# # print(sublist_sum([[1,2], [4,5,6]])) # prints 18
